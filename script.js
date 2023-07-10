8aeb234674dbfd590ed0fe765e7799736fa6f3ba
Rakibur Rahaman <mail.rakiburrahaman@gmail.com>
2023-07-11 02:03:56 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".on").click(function () {
+        $('.alert').css({
+            'transform': 'translateX(0)',
+        });
+    });
+
+    $(".close").click(function () {
+        $('.alert').css({
+            'transform': 'translateX(150%)',
+        });
+    });
+
+    $(".out").click(function () {
+        $('.alert').css({
+            'transform': 'translateX(150%)',
+        });
+    })
+})
